e9ada11731ca10303cf430d74bdbf52ce748dd26
Mark Nunnikhoven <me@markn.ca>
2021-09-23 17:09:49 -0400

    Updated Live Text project to create overlay images to show results when user adds '-o' to the command line

--- live-text/live-text.py
+++ live-text/live-text.py
@@ -3,8 +3,12 @@
 # Already created as per Google's QuickStarts:
 # Google Cloud Project: tiny-cloud-projects
 # Google Cloud Storage Bucket Name: tcp-data
+# Credentials exported as an environment variable
+#    export GOOGLE_APPLICATION_CREDENTIALS=
 
-# Already installed
+
+# Already installed from the Google Cloud SDK
+# https://cloud.google.com/python/docs/reference
 from google.cloud import storage
 from google.cloud import vision
 
@@ -13,6 +17,9 @@ import argparse
 import io
 import os
 import subprocess
+
+# 3rd party libraries
+from PIL import Image, ImageDraw # https://pillow.readthedocs.io/en/stable/installation.html
 
 def copy_to_clipboard(text):
 	"""
@@ -56,9 +63,11 @@ def main():
 	# Get the command line arguments
 	parser = argparse.ArgumentParser(description="Live Text via the CLI!")
 	parser.add_argument("-i", "--in", dest="input", required=True, help="Path to the incoming file")
+	parser.add_argument("-o", "--out", dest="output", required=False, help="Path to save the highlight file to")
 	parser.add_argument("-c", "--clipboard", dest="copy_to_clipboard", required=False, action="store_true", help="Copy the discovered text to the clipboard")
 	args = parser.parse_args()
 
+	# From the Google QuickStart...
 	# Instantiates a client
 	client = vision.ImageAnnotatorClient()
 
@@ -73,19 +82,32 @@ def main():
 
 	response = client.text_detection(image=image)
 	texts = response.text_annotations
-	print('Text found in the passed image:')
-
-	print(texts[0].description.strip())
 	results = texts[0].description.strip()
+
+	print('Text found in the passed image:')
+	print(results)
 
 	if args.copy_to_clipboard:
 		copy_to_clipboard(results)
 
-	#for text in texts:
-	#	print('\n"{}"'.format(text.description.strip()))
+	if args.output:
+		# Open the source image file
+		src_img = Image.open(args.input)
+		src_img = src_img.convert('RGBA')
+		overlay_img = Image.new('RGBA', src_img.size)
 
-	#vertices = (['({},{})'.format(vertex.x, vertex.y) for vertex in text.bounding_poly.vertices])
+		# Draw each polygon on the image as a semi-transparent layer to highlight the text
+		draw = ImageDraw.Draw(overlay_img, mode='RGBA')
+		for i, text in enumerate(texts):
+			if i == 0: continue # this text contains the entire text boundaries
+			# Polygon is in a dictionary called 'bounding_poly' 
+			xy = []
+			for v in text.bounding_poly.vertices:
+				xy.append((v.x, v.y))
 
-	#print('bounds: {}'.format(','.join(vertices)))
+			draw.polygon(xy, fill=(251, 155, 87, 72), outline=(152, 65, 4, 255))
+
+		final_img = Image.alpha_composite(src_img, overlay_img)
+		final_img.save(args.output, 'PNG')
 
 if __name__ == '__main__': main()
